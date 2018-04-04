ac839f9b2c7c060e6675eb896b99bdec59b55b3c
QiM <hokagequan@me.com>
2018-04-04 19:22:02 +0800

    Loading iqy

--- scrap_iqy/src/scrap.py
+++ scrap_iqy/src/scrap.py
@@ -1,4 +1,8 @@
-
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 def start_scrap(name):
 	print(name)
+	html = urlopen("http://www.iqiyi.com/")
+	bs_obj = BeautifulSoup(html.read(), "html.parser")
+	print(bs_obj.h2)
