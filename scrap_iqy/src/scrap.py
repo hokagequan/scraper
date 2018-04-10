db5efc56b36abb29cb4d0bdcbdd09a2a227c13f8
Quan <hokagequan@me.com>
2018-04-10 22:28:13 +0800

    code

--- scrap_iqy/src/scrap.py
+++ scrap_iqy/src/scrap.py
@@ -5,4 +5,5 @@ def start_scrap(name):
 	print(name)
 	html = urlopen("http://www.iqiyi.com/")
 	bs_obj = BeautifulSoup(html.read(), "html.parser")
-	print(bs_obj.h2)
+	search_box = bs_obj.find("input", {"class": "search-box-input"})
+	print(search_box)
