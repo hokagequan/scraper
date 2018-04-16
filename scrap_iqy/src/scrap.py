68506c4ac7acc1bab39b6019a04efcd4dae421c9
Quan <hokagequan@me.com>
2018-04-16 22:23:39 +0800

    test

--- scrap_iqy/src/scrap.py
+++ scrap_iqy/src/scrap.py
@@ -10,6 +10,7 @@ def goto_search_result_page(keyword):
 
 def start_scrap(name):
 	encode_name = quote(name)
+	encode_name = quote("肖生克的救赎")
 	url = "{0}q_{1}?source=input&sr={2}".format(constants.iqiyi_so_url, encode_name, int(time.time()))
 	html = urlopen(url)
 	bs_obj = BeautifulSoup(html.read(), "html.parser")
